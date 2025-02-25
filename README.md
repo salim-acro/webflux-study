7e5fda853f97ff621a3b9edf8c45eb5defcedb10
lim <salim@acrofuture.com>
2025-02-25 17:35:57 +0900

    수정

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # webflux-study
-webflux-study
+
+#### 2025.02.24
+환경 세팅 및 github 연결
+#### 2025.02.25
+Spring Boot webflux 컨트롤러와 테스트 만들기
+> - webflux 테스트 코드 만들기 
+>   - with @WebfluxTest
+> - get mapping
+> - get mapping with parameter
+> - Spring Boot webflux 컨트롤러와 테스트 만들기
+> - get mapping with parameter
+>   - http://localhost:8080/bye/abc
--- src/main/java/com/webflux_study/webflux_study/controller/UserController.java
+++ src/main/java/com/webflux_study/webflux_study/controller/UserController.java
@@ -1,18 +1,20 @@
 package com.webflux_study.webflux_study.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class UserController {
+
     @GetMapping("/")
-    public String getHello(){
-        // mono
-        return "hello";
+    public Mono<String> getHello(){
+        return Mono.just("Hello World");
     }
 
-    @GetMapping("/bye")
-    public String bye(){
-        return "bye";
+    @GetMapping("/bye/{name}")
+    public Mono<String> getBye(@PathVariable("name") String name){
+        return Mono.just("bye" + name);
     }
 }
